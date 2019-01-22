052e95bbbd38765fb5b4c9fecd8388edcf136500
Julio Isaac Nava Cordero <julionava@Julios-MacBook-Pro.local>
2019-01-22 11:57:31 -0700

    [ADD] Funcionalidad para mostrar ubicación y user agent del usuario

--- src/App.js
+++ src/App.js
@@ -3,26 +3,77 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      latitude: "",
+      longitude: "",
+      userAgent: ""
+    };
+
+    this.showLocationAndUserAgent();
+  }
+
+  showLocationAndUserAgent = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this.showPosition);
+    } else { 
+      this.setState({
+        latitude: "Tu navegador no soporta geolocalización",
+        longitude: "Tu navegador no soporta geolocalización"
+      });
+
+      this.showAgent();
+    }
+  }
+
+  showAgent = () => {
+    if(navigator.userAgent){
+      this.setState({
+        userAgent: "User-agent header sent: " + navigator.userAgent
+      });
+    }else{
+      this.setState({
+        userAgent: "No se pudo obtener tu user agent"
+      });
+    }
+  }
+
+
+  showPosition = (position) => {
+    this.setState({
+      latitude: position.coords.latitude + "",
+      longitude: position.coords.longitude + ""
+    });
+
+    this.showAgent();
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <button
+          className="button"
+          onClick={this.showLocationAndUserAgent}
+        >
+          Obtener información
+        </button>
+
+        <h1>Ubicación</h1>
+        <h4>Latitud</h4>
+        <p>{this.state.latitude}</p>
+
+        <h4>Longitud</h4>
+        <p>{this.state.longitude}</p>
+
+        <h1>User Agent</h1>
+        <h4>{this.state.userAgent}</h4>
       </div>
     );
   }
+
+
 }
 
 export default App;
